3712f22e9296280ec57486a24cc0bf1908ab9d54
Newsstate <newsstate6294@gmail.com>
2025-07-24 11:51:08 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,15 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import path from "path";
+import expressStatic from "express";
 
+function serveStatic(app: any) {
+  const staticPath = path.resolve(__dirname, "../client/dist");
+  app.use(expressStatic.static(staticPath));
+  app.get("*", (_req: Request, res: Response) => {
+    res.sendFile(path.join(staticPath, "index.html"));
+  });
+}
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -50,11 +58,12 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+ if (app.get("env") === "development") {
+  const { setupVite } = await import("./vite.ts");
+  await setupVite(app, server);
+} else {
+  serveStatic(app);
+}
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
